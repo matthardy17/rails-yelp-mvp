80e192d50b07fe29f519557ab515617e71008b7e
Matt Hardy <matthewhardy17@gmail.com>
2020-08-12 02:03:02 +0100

    passed all rakes and have a working app, now for some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating resturants"
+dishoom = Restaurant.new(
+    name: "Dishoom",
+    address: "Shoreditch",
+    category: "italian",
+    phone_number: "0208 462 3156"
+  )
+dishoom.save!
+
+dominos = Restaurant.new(
+    name: "Dominos",
+    address: "West Wickham",
+    category: "italian",
+    phone_number: "0208 462 3876"
+  )
+dominos.save!
+
+nandos = Restaurant.new(
+    name: "Nandos",
+    address: "Southwark",
+    category: "belgian",
+    phone_number: "0208 462 3826"
+  )
+nandos.save!
+
+wagamamas = Restaurant.new(
+    name: "Wagamamas",
+    address: "Waterloo",
+    category: "japanese",
+    phone_number: "0208 462 2853"
+  )
+wagamamas.save!
+
+ottos = Restaurant.new(
+    name: "Ottos",
+    address: "London Bridge",
+    category: "chinese",
+    phone_number: "0208 462 1001"
+  )
+ottos.save!
+puts "Finished"
