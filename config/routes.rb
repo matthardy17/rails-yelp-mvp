2d6384fc2856f00c160e293d4bdbb3266aad909b
Matt Hardy <matthewhardy17@gmail.com>
2020-08-12 03:09:37 +0100

    removed a route and embedded the review form to the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # get 'restaurants', to: 'restaurants#index'
-  # get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
-  # post 'restaurants', to: 'restaurants#create'
-  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
-  
-  resources :restaurants, only: [:index, :new, :create, :show] do
-    resources :reviews, only: [ :new, :create ]
+  resources :restaurants, only: [ :index, :new, :create, :show ] do
+    resources :reviews, only: [ :create ]
   end
-# She/He can add a new review to a restaurant
-# GET "restaurants/38/reviews/new"
-# POST "restaurants/38/reviews"
-
-
 end
